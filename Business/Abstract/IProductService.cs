c0420e52672f59c93de6ea83240ca7364ea571ad
murat coşkun <muratc2344@hotmail.com>
2022-01-19 16:37:22 +0300

    In memory data access

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        public List<Product> GetAll();
+
+    }
+}
