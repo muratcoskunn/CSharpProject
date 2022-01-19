8ac874958dc9bc72dd146462f17b460fbd9c3b27
murat coşkun <muratc2344@hotmail.com>
2022-01-19 20:11:14 +0300

    19.01.2022

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        [HttpGet]
+        public List<Product> get()
+        {
+            return productManager.GetAll();
+        }
+    }
+}
